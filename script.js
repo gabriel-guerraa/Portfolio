bae7ff085a2f71f3b61544cf706f00fb4dd51768
Gabriel Guerra <gabrielhsantos101@gmail.com>
2025-05-26 15:12:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("scrollDown").addEventListener("click", function(){
+    window.scrollTo(0, document.getElementById("project").offsetTop)
+})
+document.getElementById("ScrollDown").addEventListener("click", function () {
+    window.scrollTo(0, document.getElementById("Know").offsetTop)
+})
+document.getElementById("Scrolldown").addEventListener("click", function () {
+    window.scrollTo(0, document.getElementById("foot").offsetTop)
+})
